b12ac8746d974fc3c5dd46051c6d6f3f31327058
PHYKYSNK <3221715416@qq.com>
2026-04-22 10:30:36 +0800

    26.4.21_work

---
+++ java/JavaSe/Test.java
@@ -0,0 +1,13 @@
+class Test {
+    public static void main(String[] args) {
+        int []a1 = {1,2,3};
+        for(int i=0;i<a1.length;i++) {
+            System.out.print(a1[i] + " ");
+        }System.out.println();
+
+        int []a2 = new int[3];
+        for(int i=0;i<a1.length;i++) {
+            System.out.print(a2[i] + " ");
+        }System.out.println();
+    }
+}
